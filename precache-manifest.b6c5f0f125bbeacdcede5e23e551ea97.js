bb9b48e94237528258c05404f92f34ccefe69c20
Ayesha <ayesha.cjc@gmail.com>
2020-09-09 17:42:58 +0530

    Updates

---
+++ precache-manifest.b6c5f0f125bbeacdcede5e23e551ea97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5876a27975144543a48c3363176954a",
+    "url": "/Instagram-clone/index.html"
+  },
+  {
+    "revision": "aaddce66706deb968eaa",
+    "url": "/Instagram-clone/static/css/main.d90151d9.chunk.css"
+  },
+  {
+    "revision": "808cce7a04ae448c5280",
+    "url": "/Instagram-clone/static/js/2.4add62eb.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/Instagram-clone/static/js/2.4add62eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aaddce66706deb968eaa",
+    "url": "/Instagram-clone/static/js/main.e6ce5b83.chunk.js"
+  },
+  {
+    "revision": "81c7fea2d2f554b5b8ee",
+    "url": "/Instagram-clone/static/js/runtime-main.6943ae68.js"
+  }
+]);
